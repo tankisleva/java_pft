21fc9d6a689c92c2788bf7fb48f63d44adbd280b
Oleg <prostooleg1@gmail.com>
2016-02-26 21:22:06 +0300

    Тесты для создания контактов

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String username;
+    private final String company;
+    private final String homeadress;
+    private final String mobilenumber;
+
+    public ContactData(String firstname, String lastname, String username, String company, String homeadress, String mobilenumber) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.company = company;
+        this.homeadress = homeadress;
+        this.mobilenumber = mobilenumber;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getHomeadress() {
+        return homeadress;
+    }
+
+    public String getMobilenumber() {
+        return mobilenumber;
+    }
+}
