0377a1906e1986097e954d7993006b11b313e82c
Oleg <prostooleg1@gmail.com>
2016-03-26 12:14:54 +0300

    Реализовал передачу  аргументов в main через JCommander

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.tests.TestBase;
 
@@ -15,16 +18,36 @@ import java.util.List;
  */
 public class GroupDataGenerator {
 
-public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+    @Parameter(names = "-c",description = "Count Group")
+    public int count;
 
-    List <GroupData> groups = generateGroup(count);
-    save(groups,file);
-    
+    @Parameter(names = "-f",description = "Write File")
+    public String file;
+
+public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator =  new GroupDataGenerator();
+    JCommander jCommander =  new JCommander(generator);
+
+    try {
+    jCommander.parse(args);}
+    catch (ParameterException ex) {
+        jCommander.usage();
+        return;
+    }
+    generator.run();
+
+
+//    int count = Integer.parseInt(args[0]);
+//    File file = new File(args[1]);
+
 }
 
-    private static void save(List<GroupData> groups, File file) throws IOException {
+    private void run() throws IOException {
+        List <GroupData> groups = generateGroup(count);
+        save(groups,new File(file));
+    }
+
+    private  void save(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
 
         for (GroupData group: groups){
@@ -33,7 +56,7 @@ public static void main(String[] args) throws IOException {
          writer.close();
     }
 
-    private static List<GroupData> generateGroup(int count) {
+    private  List<GroupData> generateGroup(int count) {
         List <GroupData> groups =  new ArrayList<GroupData>();
 
         for (int i = 0; i<count; i++){
