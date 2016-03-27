42c3ddecf4c55ea43cff7e94c29740a5e90fea38
Oleg <prostooleg1@gmail.com>
2016-03-27 12:08:33 +0300

    Реализовал логирование старта и остановки тестовых методов через logback

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -2,7 +2,6 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
@@ -10,7 +9,10 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,6 +23,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class GroupCreationTests extends TestBase {
+
+
 
 
     @DataProvider
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,9 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.helpers.ApplicationManager;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 /**
  * Created by oleg on 26.02.16.
@@ -11,7 +19,7 @@ import ru.stqa.pft.addressbook.helpers.ApplicationManager;
 public class TestBase {
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser",BrowserType.FIREFOX));
 
-
+    Logger loger = LoggerFactory.getLogger(TestBase.class);
 
 
     @BeforeSuite
@@ -20,11 +28,18 @@ public class TestBase {
     }
 
 
-
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+     @BeforeMethod
+    public void logTestStart(Method m,Object[] p){
+         loger.info("Start test " + m.getName() + " with parameters "+ Arrays.asList(p));
+     }
 
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m){
+        loger.info("End test " + m.getName());
+    }
 }
