879cf7b237a206869a7dc507c5b715fc3b1e3833
Oleg <prostooleg1@gmail.com>
2016-04-20 10:45:42 +0300

    Реализовал тест с использованием библиотеки RestAssured

---
+++ rest-sample/src/test/java/ru/stqa/pft/tests/RestAssuredTests.java
@@ -0,0 +1,62 @@
+package ru.stqa.pft.tests;
+
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.jayway.restassured.RestAssured;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by oleg on 20.04.16.
+ */
+public class RestAssuredTests {
+
+    @BeforeClass
+    public void init(){
+        RestAssured.authentication = RestAssured.basic("LSGjeU4yP1X493ud1hNniA==", "");
+    }
+
+    @Test
+    public void testCreateIssue() throws IOException {
+        Set<Issue> oldIssues = getIssues();
+        Issue newIssue = new Issue().withSubject("Test issue").withDescription("New test issue");
+        int issueId =  create(newIssue);
+        Set <Issue> newIssues = getIssues();
+        oldIssues.add(newIssue.whitId(issueId));
+        assertEquals(newIssues,oldIssues);
+
+
+    }
+
+    private Set<Issue> getIssues() throws IOException {
+        String json = RestAssured.get("http://demo.bugify.com/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        JsonElement issues = parsed.getAsJsonObject().get("issues");
+        return new Gson().fromJson(issues,new TypeToken<Set<Issue>>(){}.getType());
+
+    }
+
+    
+
+    private int create(Issue issue) throws IOException {
+        String json =   RestAssured.given().parameter("subject", issue.getSubject())
+                .parameter("description",issue.getDescription())
+                .post("http://demo.bugify.com/api/issues.json").asString();
+        JsonElement parsed = new JsonParser().parse(json);
+        return parsed.getAsJsonObject().get("issue_id").getAsInt();
+    }
+
+
+}
+
