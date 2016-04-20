e93c25a1100fe1d2dba3b744bdb47ee041c214ee
Oleg <prostooleg1@gmail.com>
2016-04-20 10:25:49 +0300

    Реализовал взаимодействие с Bugify через REST

---
+++ rest-sample/src/test/java/ru/stqa/pft/tests/Issue.java
@@ -0,0 +1,61 @@
+package ru.stqa.pft.tests;
+
+/**
+ * Created by oleg on 20.04.16.
+ */
+public class Issue {
+
+    private int id;
+    private String subject;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public Issue whitId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public Issue withSubject(String subject) {
+        this.subject = subject;
+        return this;
+    }
+
+
+    public Issue withDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Issue issue = (Issue) o;
+
+        if (id != issue.id) return false;
+        if (!subject.equals(issue.subject)) return false;
+        return description.equals(issue.description);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + subject.hashCode();
+        result = 31 * result + description.hashCode();
+        return result;
+    }
+}
